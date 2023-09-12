61f7b045fed6c812baada289610b5c829922394c
JechaTinashe <jechatinashe7@gmail.com>
2023-09-12 08:46:04 +0200

    workspace

--- screens/Discover.js
+++ screens/Discover.js
@@ -1,191 +1,229 @@
-import { View, Text, SafeAreaView, Image, ScrollView, TouchableOpacity, ActivityIndicator,RefreshControl,FlatList,StyleSheet, Linking } from 'react-native'
-import React, { useLayoutEffect, useState, useEffect  } from 'react'
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import React, { useLayoutEffect, useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+  Linking,
+  RefreshControl,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { Avatar, Residence, Attractions, Chats } from '../assets';
-import MenuContainer from '../components/MenuContainer';
 import { FontAwesome } from '@expo/vector-icons';
-import {collection, getDocs,doc,setDoc,orderBy, limit,query,startAfter } from 'firebase/firestore/lite';
+import { collection, getDocs, query, orderBy, limit, startAfter } from 'firebase/firestore/lite';
+
 import { db } from '../FirebaseConfig/Firebase';
 
-
 const Discover = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    const [Loading,setLoading] = useState(false);
-    const [isReloading,SetReloading] = useState(false);
-    const RequestsRef = collection(db, 'Tbl_Residences');
-    const [Requets,SetRequests] = useState([]);
-    const [LastDoc,SetLastDoc] = useState(null);
-  
-    useEffect(() => {
-      GetRequests();
-    },[]);
+  const [isLoading, setLoading] = useState(false);
+  const [isReloading, setReloading] = useState(false);
+  const RequestsRef = collection(db, 'Tbl_Residences');
+  const [Requests, setRequests] = useState([]);
+  const [lastDoc, setLastDoc] = useState(null);
 
-    const GetRequests = async () =>{
-      setLoading(true);
-      const Snapshot = await getDocs(query(RequestsRef,orderBy("Pricing", "desc"),limit(5)));
-  
-      if(!Snapshot.empty){
-        let NewRequests = [];
-  
-        SetLastDoc(Snapshot.docs[Snapshot.docs.length - 1]);
-  
-        for(let i = 0; i< Snapshot.docs.length;i++ ){
-          NewRequests.push(Snapshot.docs[i].data());
-        }
-  
-        SetRequests(NewRequests);
-  
+  useEffect(() => {
+    getRequests();
+  }, []);
+
+  const getRequests = async () => {
+    setLoading(true);
+    const Snapshot = await getDocs(query(RequestsRef, orderBy('Pricing', 'desc'), limit(5)));
+
+    if (!Snapshot.empty) {
+      let newRequests = [];
+
+      setLastDoc(Snapshot.docs[Snapshot.docs.length - 1]);
+
+      for (let i = 0; i < Snapshot.docs.length; i++) {
+        newRequests.push(Snapshot.docs[i].data());
       }
-      else{
-        SetLastDoc(null);
-      }
-  
-      setLoading(false);
-  
-    };
-  
-    const GetMoreRequests = async () => {
-      setLoading(true);
-      if(LastDoc){
-        let Snapshot = await getDocs(query(RequestsRef,orderBy("Pricing", "desc"),startAfter(LastDoc.data().CreatedOn),limit(5)));
-        if(!Snapshot.empty){
-          
-          SetLastDoc(Snapshot.docs[Snapshot.docs.length - 1]);
-    
-          for(let i = 0; i< Snapshot.docs.length;i++ ){
-            Requets.push(Snapshot.docs[i].data());
-          }
-    
-        }
-        else{
-          SetLastDoc(null);
-        }
-  
-      }
-      setLoading(false);
+
+      setRequests(newRequests);
+    } else {
+      setLastDoc(null);
     }
-  
-    const onRefresh = () => {
-      GetRequests()
-      SetReloading(false);
-    };
-  
-    const [type, setType] = useState("chats")
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerShown: false,
-        });
+    setLoading(false);
+  };
 
-    },[]);
-  
-    return (
-      <SafeAreaView  style={{ flex: 1, backgroundColor: 'white' }}>
-        
-        <View className="flex-row items-center justify-between px-8">
-      <View>
-            <Text className="text-[40px] text-[#f9193e] font-bold">Res 4 U</Text>
-            <Text className="text-[23px] text-[#527283]">Discover Your student accommodation</Text>
+  const getMoreRequests = async () => {
+    setLoading(true);
+    if (lastDoc) {
+      let Snapshot = await getDocs(
+        query(RequestsRef, orderBy('Pricing', 'desc'), startAfter(lastDoc.data().CreatedOn), limit(5))
+      );
+      if (!Snapshot.empty) {
+        setLastDoc(Snapshot.docs[Snapshot.docs.length - 1]);
+
+        for (let i = 0; i < Snapshot.docs.length; i++) {
+          Requests.push(Snapshot.docs[i].data());
+        }
+      } else {
+        setLastDoc(null);
+      }
+    }
+    setLoading(false);
+  };
+
+  const onRefresh = () => {
+    getRequests();
+    setReloading(false);
+  };
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+      <View style={styles.header}>
+        <View>
+          <Text style={styles.headerText}>Res 4 U</Text>
+          <Text style={styles.subHeaderText}>Discover Your Student Accommodation</Text>
         </View>
 
-        <View className="w-12 h-12 bg-gray-400 rounded-md items-center justify-center shadow-lg">
-          <Image
-            source={Avatar}
-            className="w-full h-full rounded-md object-cover"
-          />
+        <View style={styles.avatarContainer}>
+          <Image source={Avatar} style={styles.avatar} />
         </View>
-      </View>       
+      </View>
+
       <View style={styles.circleButtonsContainer}>
-                <TouchableOpacity
-                    style={styles.circleButton}
-                    onPress={() => navigation.navigate('InternalNav', { screen: 'Attraction' })}>
-                    <FontAwesome name="university" size={30} color="#f9193e" />
-                    <Text style={styles.circleButtonText}>Attractions</Text>
-                </TouchableOpacity>
-                {/* <TouchableOpacity
-                    style={styles.circleButton}
-                    onPress={() => navigation.navigate('ChatPage')}>
-                    <FontAwesome name="comments" size={30} color="#f9193e" />
-                    <Text style={styles.circleButtonText}>Chat</Text>
-                </TouchableOpacity> */}
-                <TouchableOpacity
-                    style={styles.circleButton}
-                    onPress={() => Linking.openURL('www.google.com')}>
-                    <FontAwesome name="globe" size={30} color="#f9193e" />
-                    <Text style={styles.circleButtonText}>Res4U Website</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.circleButton}
-                  onPress={() => navigation.navigate('InternalNav', { screen: 'Game' })}>
-                     <FontAwesome name="gamepad" size={30} color="#f9193e" />
-                  <Text style={styles.circleButtonText}>Game</Text>
-                </TouchableOpacity>
-            </View>
+        <TouchableOpacity
+          style={styles.circleButton}
+          onPress={() => navigation.navigate('InternalNav', { screen: 'Attraction' })}>
+          <FontAwesome name="university" size={30} color="#f9193e" />
+          <Text style={styles.circleButtonText}>Attractions</Text>
+        </TouchableOpacity>
 
-            <View style={styles.headingContainer}>
-                <Text style={styles.headingText}>Residences:</Text>
-            </View>
-        <FlatList
-          contentContainerStyle={styles.flatListContent}
-          data={Requets}
-          numColumns={2} // Display two columns
-          refreshControl={<RefreshControl refreshing={isReloading} onRefresh={onRefresh} />}
-          onEndReached={GetMoreRequests}
-          onEndReachedThreshold={0.01}
-          scrollEventThrottle={150}
-          keyExtractor={(item) => item.id} // Use a unique key for each item
-          renderItem={({ item }) => (
-            <View style={styles.itemContainer}>
-              <TouchableOpacity style={styles.itemTouchable}
-              onPress={() => navigation.navigate('Res', { item })} 
-              >
-                <Image source={{ uri: item.img }} style={styles.itemImage} />
-                <Text style={styles.itemName}>{item.Res_Name}</Text>
-                <View style={styles.itemDetailsContainer}>
-                  <Text style={styles.itemDetailLabel}>Area:</Text>
-                  <Text style={styles.itemDetail}>{item.Area}</Text>
-                </View>
-                <View style={styles.itemDetailsContainer}>
-                  <Text style={styles.itemDetailLabel}>Pricing:</Text>
-                  <Text style={styles.itemDetail}>R {item.Pricing}</Text>
-                </View>
-                <View style={styles.itemDetailsContainer}>
-                  <Text style={styles.itemDetailLabel}>Rooms:</Text>
-                  <Text style={styles.itemDetail}>{item.Rooms}</Text>
-                </View>
-              </TouchableOpacity>
-            </View>
-          )}
-        />
-      </SafeAreaView>
-    )
-    
+        <TouchableOpacity
+          style={styles.circleButton}
+          onPress={() => Linking.openURL('www.google.com')}>
+          <FontAwesome name="globe" size={30} color="#f9193e" />
+          <Text style={styles.circleButtonText}>Res4U Website</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.circleButton}
+          onPress={() => navigation.navigate('InternalNav', { screen: 'Game' })}>
+          <FontAwesome name="gamepad" size={30} color="#f9193e" />
+          <Text style={styles.circleButtonText}>Game</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.headingContainer}>
+        <Text style={styles.headingText}>Residences:</Text>
+      </View>
+
+      <FlatList
+        contentContainerStyle={styles.flatListContent}
+        data={Requests}
+        numColumns={2} // Display two columns
+        refreshControl={<RefreshControl refreshing={isReloading} onRefresh={onRefresh} />}
+        onEndReached={getMoreRequests}
+        onEndReachedThreshold={0.01}
+        scrollEventThrottle={150}
+        keyExtractor={(item) => item.id.toString()} // Use a unique key for each item
+        renderItem={({ item }) => (
+          <View style={styles.itemContainer}>
+            <TouchableOpacity
+              style={styles.itemTouchable}
+              onPress={() => navigation.navigate('Res', { item })}>
+              <Image source={{ uri: item.img }} style={styles.itemImage} />
+              <Text style={styles.itemName}>{item.Res_Name}</Text>
+              <View style={styles.itemDetailsContainer}>
+                <Text style={styles.itemDetailLabel}>Area:</Text>
+                <Text style={styles.itemDetail}>{item.Area}</Text>
+              </View>
+              <View style={styles.itemDetailsContainer}>
+                <Text style={styles.itemDetailLabel}>Pricing:</Text>
+                <Text style={styles.itemDetail}>R {item.Pricing}</Text>
+              </View>
+              <View style={styles.itemDetailsContainer}>
+                <Text style={styles.itemDetailLabel}>Rooms:</Text>
+                <Text style={styles.itemDetail}>{item.Rooms}</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 16,
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: '#f9193e',
+  },
+  subHeaderText: {
+    fontSize: 23,
+    color: '#527283',
+  },
+  avatarContainer: {
+    width: 60,
+    height: 60,
+    backgroundColor: 'gray',
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+  },
+  avatar: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 30,
+  },
+  circleButtonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    paddingVertical: 20,
+  },
+  circleButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  circleButtonText: {
+    marginTop: 5,
+    color: '#527283',
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+  headingContainer: {
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  headingText: {
+    fontSize: 27,
+    fontWeight: 'bold',
+    color: '#333',
+  },
   flatListContent: {
     paddingTop: 16,
     paddingHorizontal: 16,
   },
-
-  headingContainer: {
-    alignItems: 'center',
-    paddingVertical: 10,
-},
-headingText: {
-    fontSize: 27,
-    fontWeight: 'bold',
-    color: '#333',
-    
-},
   itemContainer: {
     flex: 0.5, // Display two columns
     justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 20,
-    marginHorizontal:10,
+    marginHorizontal: 10,
   },
   itemTouchable: {
     backgroundColor: '#fff', // White background
@@ -226,23 +264,6 @@ headingText: {
     fontSize: 14,
     color: '#777',
   },
-
-  circleButtonsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    paddingVertical: 20,
-},
-circleButton: {
-    justifyContent: 'center',
-    alignItems: 'center',
-},
-circleButtonText: {
-    marginTop: 5,
-    color: '#527283',
-    fontSize: 12,
-    fontWeight: 'bold',
-},
 });
 
-export default Discover
+export default Discover;
